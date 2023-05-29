f05860961af6f4a51516d1d4060d7b31e1afe416
EllaKaye <ella.kaye@gmail.com>
2023-05-29 12:03:44 +0100

    size_t_test

---
+++ Session 2/size_t_test.c
@@ -0,0 +1,13 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+  size_t a = 5;
+  size_t b = 7;
+
+  printf("The difference of a and b is %zu", a - b);
+
+  return EXIT_SUCCESS;
+}
